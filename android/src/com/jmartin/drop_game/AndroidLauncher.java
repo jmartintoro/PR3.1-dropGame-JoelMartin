bc97b6e84d60967c2d08939f712513a175530c44
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-03-12 18:37:07 +0100

    first commit

---
+++ android/src/com/jmartin/drop_game/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.jmartin.drop_game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jmartin.drop_game.dropGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new dropGame(), config);
+	}
+}
---
+++ ios/src/com/jmartin/drop_game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.jmartin.drop_game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.jmartin.drop_game.dropGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new dropGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
